dfd40d7a8fb3eedf26ee8c1dc0d6cfd07bd8dca3
Domenic Beall <domenicbeall2@gmail.com>
2021-04-10 16:13:38 +1000

    Adding routes for user management

---
+++ routes/user-routes.js
@@ -0,0 +1,8 @@
+const jwt = require("jsonwebtoken");
+const jwtExpiryTime = 86400; // The expiry time of the login cookie (in seconds), set to a default of 24 hours
+
+module.exports = (app) => {
+
+    
+
+}
