964ea7595fe23edf6148dffddff034010dc01741
DomenicBeall <domenicbeall2@gmail.com>
2021-04-11 21:14:45 +1000

    Began adding client side auth

---
+++ client/src/context/auth.js
@@ -0,0 +1,58 @@
+import React, {useReducer, createContext} from 'react';
+
+const initialState = {
+    user: { username: "steve" }
+};
+
+// TODO: If the token is in the cookies, decode it, check if it's still valid and set the user in the initialstate
+
+const AuthContext = createContext({
+    user: null,
+    login: (userData) => {},
+    logout: () => {}
+});
+
+function authReducer(state, action) {
+    switch (action.type) {
+        case 'LOGIN':
+            return {
+                ...state,
+                user: action.payload
+            };
+        case 'LOGOUT':
+            return {
+                ...state,
+                user: null
+            };
+        default:
+            return state;
+    }
+}
+
+function AuthProvider(props) {
+    const [ state, dispatch ] = useReducer(authReducer, initialState);
+
+    console.log(state);
+
+    function login(userData) {
+        dispatch({
+            type: 'LOGIN',
+            payload: userData
+        });
+    }
+
+    function logout() {
+        // TODO: Remove the token from cookies
+        dispatch({ type: 'LOGOUT' });
+    }
+
+    return (
+        <AuthContext.Provider
+            value={{ user: state.user, login, logout }}
+            {...props}
+        />
+    )
+}
+
+export {AuthContext, AuthProvider};
+
